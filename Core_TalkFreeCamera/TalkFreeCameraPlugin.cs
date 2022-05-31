7f9182145be0957bb810b01fdf755c73dec5876d
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2022-05-31 09:51:29 +0200

    Refactoring; Use existing methods if possible

--- Core_TalkFreeCamera/TalkFreeCameraPlugin.cs
+++ Core_TalkFreeCamera/TalkFreeCameraPlugin.cs
@@ -2,7 +2,7 @@
 
 using BepInEx;
 using HarmonyLib;
-
+using KKAPI;
 using KKAPI.MainGame;
 using KKAPI.Utilities;
 
@@ -10,12 +10,15 @@ using UnityEngine;
 
 namespace KK_TalkFreeCamera
 {
-    [BepInProcess("Koikatu")]
-    [BepInProcess("Koikatsu Party")]
-    [BepInPlugin(nameof(KK_TalkFreeCamera), nameof(KK_TalkFreeCamera), VERSION)]
-    public class KK_TalkFreeCamera : BaseUnityPlugin
+    [BepInProcess(KoikatuAPI.GameProcessName)]
+    [BepInProcess(KoikatuAPI.GameProcessNameSteam)]
+    [BepInDependency(KoikatuAPI.GUID, KoikatuAPI.VersionConst)]
+    [BepInPlugin(GUID, PluginName, Version)]
+    public class TalkFreeCameraPlugin : BaseUnityPlugin
     {
-        public const string VERSION = "1.0.0";
+        public const string GUID = "KK_TalkFreeCamera";
+        public const string PluginName = "TalkFreeCamera";
+        public const string Version = "1.0.0";
 
         private static TalkScene talkScene;
         private static SaveData.Heroine heroine;
@@ -23,19 +26,18 @@ namespace KK_TalkFreeCamera
 
         private void Awake()
         {
-            var icon = GetIcon();
-            if (icon == null)
-                return;
-            
+            var icon = ResourceUtils.GetEmbeddedResource("cam.png", typeof(TalkFreeCameraPlugin).Assembly).LoadTexture().ToSprite();
             GameAPI.AddTouchIcon(icon, button => button.onClick.AddListener(ToggleCameraMode));
-            Harmony.CreateAndPatchAll(typeof(KK_TalkFreeCamera));
+
+            Harmony.CreateAndPatchAll(typeof(TalkFreeCameraPlugin), GUID);
         }
-        
+
         private static void ToggleCameraMode()
         {
+            var talkScene = GameAPI.GetTalkScene();
             if (ccv2 == null || talkScene == null)
                 return;
-            
+
             heroine = talkScene.targetHeroine;
             if (heroine == null)
                 return;
@@ -47,50 +49,30 @@ namespace KK_TalkFreeCamera
             else
                 ccv2.Reset(1);
         }
-
-        private static Sprite GetIcon()
-        {
-            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"{nameof(KK_TalkFreeCamera)}.Resources.cam.png"))
-            {
-                if (stream == null) 
-                    return null;
-                
-                var bytesInStream = new byte[stream.Length];
-                stream.Read(bytesInStream, 0, bytesInStream.Length);
-
-                if (bytesInStream.Length == 0)
-                    return null;
-                
-                var texture = new Texture2D(64, 64);
-                texture.LoadImage(bytesInStream);
-
-                return texture == null ? null : texture.ToSprite();
-            }
-        }
-
+        
         [HarmonyPostfix, HarmonyPatch(typeof(TalkScene), "Awake")]
         private static void TalkScene_Awake_Postfix(TalkScene __instance)
         {
             talkScene = __instance;
 
-            if (Camera.main == null) 
+            if (Camera.main == null)
                 return;
-            
+
             ccv2 = Camera.main.gameObject.AddComponent<CameraControl_Ver2>();
             if (ccv2 == null)
                 return;
-            
+
             ccv2.enabled = false;
         }
-        
+
         [HarmonyPrefix, HarmonyPatch(typeof(TalkScene), "OnDestroy")]
         private static void TalkScene_OnDestroy_Prefix()
         {
             if (ccv2 == null)
                 return;
-            
+
             Destroy(ccv2);
-            
+
             heroine = null;
             ccv2 = null;
         }
